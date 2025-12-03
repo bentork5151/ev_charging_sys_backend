5f2a4ca4581fb5aca007b740fe48d04f6893edc3
jayeshmahajn <jayeshmahajan340@gmail.com>
2025-12-03 15:31:00 +0530

    add in test

--- src/test/java/com/bentork/evchargingsystem/EvChargingSystemApplicationTests.java
+++ src/test/java/com/bentork/evchargingsystem/EvChargingSystemApplicationTests.java
@@ -3,7 +3,9 @@ package com.bentork.evchargingsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.bentork.ev_system.EvChargingSystemApplication;
+
+@SpringBootTest(classes = EvChargingSystemApplication.class)
 class EvChargingSystemApplicationTests {
 
 	@Test
